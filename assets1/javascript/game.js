a1de884dd4f40c41516baa1463522f4a55c69730
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-04-23 10:51:20 -0400

    first attempt with primitive functionality.

---
+++ assets1/javascript/game.js
@@ -0,0 +1,3 @@
+var martialArts = ["JiuJitsu", "Judo", "Karate", "TaeKwonDo", "KungFu", "KravMaga", "NipponKempo", "Systema", "WingTsun", "MuayThai"];
+var martialArtIndex = Math.floor(Math.random()*martialArts.length);
+var playWord = martialArts[martialArtIndex];
